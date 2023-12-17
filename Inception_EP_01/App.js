824231e1f857d3ea35a434f234fe8bceafa855a4
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-12-17 23:43:17 +0530

    multiple children to a single parent

--- Inception_EP_01/App.js
+++ Inception_EP_01/App.js
@@ -1,10 +1,15 @@
-const heading = React.createElement(
+const heading1 = React.createElement(
 	"h1",
 	{ id: "Heading" },
 	"Hello World from React"
 );
-
-console.log(heading); // Object
+const heading2 = React.createElement(
+	"h2",
+	{ id: "heading" },
+	"Hello World from React h2"
+);
+const child = React.createElement("div", { id: "child" }, [heading1, heading2]);
+const parent = React.createElement("div", { id: "parent" }, child);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
